008ccb0ba6f5b45fb29c06172bd37bd57d19d084
bahadylbekov <33404905+bahadylbekov@users.noreply.github.com>
2019-08-03 00:02:27 +0100

    add: specify main theme constants, add text component

---
+++ components/Text.js
@@ -0,0 +1,147 @@
+import React, { Component } from "react";
+import { Text, StyleSheet } from "react-native";
+
+import { theme } from "../constants";
+
+export default class Typography extends Component {
+  render() {
+    const {
+      h1,
+      h2,
+      h3,
+      h4,
+      headline,
+      body,
+      callout,
+      subhead,
+      footnote,
+      caption,
+      caption2,  
+      size,
+      transform,
+      align,
+      // styling
+      regular,
+      bold,
+      semibold,
+      medium,
+      weight,
+      light,
+      center,
+      right,
+      spacing, // letter-spacing
+      height, // line-height
+      // colors
+      color,
+      black,
+      notBlack,
+      lightGray,
+      gray,
+      darkGray,
+      white,
+      lightBlue,
+      blue,
+      lightSkyBlue,
+      red,
+      pink,
+      style,
+      children,
+      ...props
+    } = this.props;
+
+    const textStyles = [
+      styles.text,
+      h1 && styles.h1,
+      h2 && styles.h2,
+      h3 && styles.h3,
+      title && styles.title,
+      body && styles.body,
+      caption && styles.caption,
+      size && { fontSize: size },
+      transform && { textTransform: transform },
+      align && { textAlign: align },
+      height && { lineHeight: height },
+      spacing && { letterSpacing: spacing },
+      weight && { fontWeight: weight },
+      regular && styles.regular,
+      bold && styles.bold,
+      semibold && styles.semibold,
+      medium && styles.medium,
+      light && styles.light,
+      center && styles.center,
+      right && styles.right,
+      color && styles[color],
+      color && !styles[color] && { color },
+      // color shortcuts
+      black && styles.black,
+      notBlack && styles.notBlack,
+      lightGray && styles.lightGray,
+      grey && styles.grey,
+      darkGray && styles.darkGray,
+      white && styles.white,
+      lightBlue && styles.lightBlue,
+      blue && styles.blue,
+      lightSkyBlue && styles.lightSkyBlue,
+      red && styles.red,
+      pink && styles.pink,
+      style // rewrite predefined styles
+    ];
+
+    return (
+      <Text style={textStyles} {...props}>
+        {children}
+      </Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  // default style
+  text: {
+    fontSize: theme.sizes.font,
+    color: theme.colors.black
+  },
+  // variations
+  regular: {
+    fontWeight: "normal",
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  semibold: {
+    fontWeight: "500",
+  },
+  medium: {
+    fontWeight: "500",
+  },
+  light: {
+    fontWeight: "200",
+  },
+  // position
+  center: { textAlign: "center" },
+  right: { textAlign: "right" },
+  // colors
+  black: { color: theme.colors.black },
+  notBlack: { color: theme.colors.notBlack },
+  lightGray: { color: theme.colors.lightGray },
+  gray: { color: theme.colors.gray },
+  darkGray: { color: theme.colors.darkGrey },
+  white: { color: theme.colors.white },
+  lightBlue: { color: theme.colors.lightBlue },
+  blue: { color: theme.colors.blue },
+  lightSkyBlue: { color: theme.colors.lightSkyBlue },
+  red: { color: theme.colors.red },
+  pink: { color: theme.colors.pink },
+  // fonts
+  h1: theme.fonts.h1,
+  h2: theme.fonts.h2,
+  h3: theme.fonts.h3,
+  h4: theme.fonts.h4,
+  headline: theme.fonts.headline,
+  body: theme.fonts.body,
+  callout: theme.fonts.callout,
+  subhead: theme.fonts.subhead,
+  footnote: theme.fonts.footnote,
+  caption: theme.fonts.caption,
+  caption2: theme.fonts.caption2,  
+});
---
+++ constants/theme.js
@@ -0,0 +1,74 @@
+const colors = {
+    background: "#FFFFFF",
+    blueBackground: "#EEF6FF",
+    black: "#000000",
+    notBlack: "#333333",
+    lightGray: "#EBEBEB",
+    gray: "#989898",
+    darkGrey: "#5C5C5C",
+    white: "#FFFFFF",
+    lightBlue: "#F9FCFF",
+    blue: "#007AFF",
+    lightSkyBlue: "#90C5FF",
+    red: "#FF6161",
+    pink: "#F4F6FF",
+  };
+  
+  const sizes = {
+    // global sizes
+    base: 16,
+    font: 14,
+    radius: 6,
+    padding: 25,
+  
+    // font sizes
+    h1: 34,
+    h2: 28,
+    h3: 22,
+    h4: 20,
+    headline: 17,
+    body: 17,
+    callout: 16,
+    subhead: 15,
+    footnote: 13,
+    caption: 12,
+    caption2: 11,
+  };
+  
+  const fonts = {
+    h1: {
+      fontSize: sizes.h1
+    },
+    h2: {
+      fontSize: sizes.h2
+    },
+    h3: {
+      fontSize: sizes.h3
+    },
+    h4: {
+      fontSize: sizes.h4
+    },  
+    headline: {
+      fontSize: sizes.headline
+    },
+    body: {
+        fontSize: sizes.body
+      },
+    callout: {
+      fontSize: sizes.callout
+    },
+    subhead: {
+        fontSize: sizes.subhead
+    },
+    footnote: {
+        fontSize: sizes.footnote
+    },
+    caption: {
+        fontSize: sizes.caption
+    },
+    caption2: {
+      fontSize: sizes.caption2
+    },
+  };
+  
+  export { colors, sizes, fonts };
