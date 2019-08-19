376e3f8c2b5806dcdc65792eeeb4a554e4559b1f
ChronosX88 <chronosx88@gmail.com>
2019-08-19 18:03:08 +0400

    fix: Make getDirectChats method non-blocking

--- android/app/src/main/java/com/moonshrd/MatrixClientModule.kt
+++ android/app/src/main/java/com/moonshrd/MatrixClientModule.kt
@@ -3,6 +3,8 @@ package com.moonshrd
 import com.facebook.react.bridge.*
 import com.google.gson.Gson
 import com.moonshrd.model.DirectChatModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.lang.RuntimeException
 
 class MatrixClientModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
@@ -12,36 +14,41 @@ class MatrixClientModule(reactContext: ReactApplicationContext) : ReactContextBa
 
     @ReactMethod
     fun getDirectChats(promise: Promise) {
-        val matrixInstance = Matrix.getInstance(reactApplicationContext)
-        if(matrixInstance.defaultSession == null) {
-            promise.reject(RuntimeException("Session must not be null!"))
-        }
-        val directChats = matrixInstance.defaultSession.dataHandler.store.rooms.filter {
-            it.isDirect
-        }
-
-        for(i in 0 until 15) {
-            if(!Globals.State.isInitialSyncComplete) {
-                Thread.sleep(1000)
+        GlobalScope.launch {
+            val matrixInstance = Matrix.getInstance(reactApplicationContext)
+            if(matrixInstance.defaultSession == null) {
+                promise.reject(RuntimeException("Session must not be null!"))
             }
-            break
-        }
+            val directChats = matrixInstance.defaultSession.dataHandler.store.rooms.filter {
+                it.isDirect
+            }
 
-        val chatModels = mutableListOf<DirectChatModel>()
-        directChats.forEach { room ->
-            val chat = DirectChatModel()
-            chat.name = room.getRoomDisplayName(reactApplicationContext)
-            chat.avatarUri = room.avatarUrl ?: ""
-            val roomSummary = matrixInstance.defaultSession.dataHandler.store.getSummary(room.roomId)
-            val contactID = roomSummary!!.heroes.filter {
-                it != matrixInstance.defaultSession.myUserId
-            }[0]
-            val contact = matrixInstance.defaultSession.dataHandler.store.getUser(contactID)
-            chat.lastSeen = contact.latestPresenceTs
-            chat.isActive = contact.isActive
-            chatModels.add(DirectChatModel())
+            for(i in 0 until 15) {
+                if(!Globals.State.isInitialSyncComplete) {
+                    if(i == 14) {
+                        promise.reject(RuntimeException("Sync waiting timeout!"))
+                    }
+                    Thread.sleep(1000)
+                }
+                break
+            }
+
+            val chatModels = mutableListOf<DirectChatModel>()
+            directChats.forEach { room ->
+                val chat = DirectChatModel()
+                chat.name = room.getRoomDisplayName(reactApplicationContext)
+                chat.avatarUri = room.avatarUrl ?: ""
+                val roomSummary = matrixInstance.defaultSession.dataHandler.store.getSummary(room.roomId)
+                val contactID = roomSummary!!.heroes.filter {
+                    it != matrixInstance.defaultSession.myUserId
+                }[0]
+                val contact = matrixInstance.defaultSession.dataHandler.store.getUser(contactID)
+                chat.lastSeen = contact.latestPresenceTs
+                chat.isActive = contact.isActive
+                chatModels.add(DirectChatModel())
+            }
+            val gson = Gson()
+            promise.resolve(gson.toJson(chatModels))
         }
-        val gson = Gson()
-        promise.resolve(gson.toJson(chatModels))
     }
 }
