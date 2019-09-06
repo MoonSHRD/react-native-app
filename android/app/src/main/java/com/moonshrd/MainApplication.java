6c703eb1d3b38326aca47b0f56edf684fb18427f
ChronosX88 <chronosx88@gmail.com>
2019-09-06 19:33:51 +0400

    clean: Cleanup MainApplication class

--- android/app/src/main/java/com/moonshrd/MainApplication.java
+++ android/app/src/main/java/com/moonshrd/MainApplication.java
@@ -7,19 +7,19 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 
-import com.facebook.react.ReactApplication;
-import com.actionsheet.ActionSheetPackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.moonshrd.services.P2ChatService;
-import com.moonshrd.utils.matrix.Matrix;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.horcrux.svg.SvgPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
+import com.actionsheet.ActionSheetPackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.horcrux.svg.SvgPackage;
+import com.moonshrd.services.P2ChatService;
+import com.moonshrd.utils.matrix.Matrix;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 
 import org.matrix.androidsdk.MXSession;
 
@@ -28,17 +28,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import io.realm.Realm;
-import io.realm.RealmConfiguration;
-
 public class MainApplication extends Application implements ReactApplication {
-  private static Matrix matrixInstance = null;
-
   public static final String SERVICE_TOPIC = "moonshard";
   public static final String PROTOCOL_ID = "/moonshard/1.0.0";
-    public static ServiceConnection serviceConnection = null;
-    public static P2ChatService service = null;
-    public Context context = null;
+  public static ServiceConnection serviceConnection = null;
+  public static P2ChatService service = null;
+  public Context context = null;
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -48,7 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
+      return Arrays.asList(
           new MainReactPackage(),
             new ActionSheetPackage(),
             new PickerPackage(),
@@ -75,41 +70,26 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    matrixInstance = Matrix.getInstance(getApplicationContext());
 
-      context = getApplicationContext();
+    context = getApplicationContext();
 
-      this.context.startService(new Intent(this.context, P2ChatService.class));
+    this.context.startService(new Intent(this.context, P2ChatService.class));
 
-      ServiceConnection serviceConnection = new ServiceConnection() {
-          @Override
-          public void onServiceConnected(ComponentName name, IBinder service) {
-              MainApplication.serviceConnection = this;
-              MainApplication.service = ((P2ChatService.P2ChatServiceBinder) service).getService();
-          }
+    ServiceConnection serviceConnection = new ServiceConnection() {
+      @Override
+      public void onServiceConnected(ComponentName name, IBinder service) {
+        MainApplication.serviceConnection = this;
+        MainApplication.service = ((P2ChatService.P2ChatServiceBinder) service).getService();
+      }
 
-          @Override
-          public void onServiceDisconnected(ComponentName name) {
-              MainApplication.serviceConnection = null;
-              MainApplication.service = null;
-          }
-      };
+      @Override
+      public void onServiceDisconnected(ComponentName name) {
+        MainApplication.serviceConnection = null;
+        MainApplication.service = null;
+      }
+    };
 
-      this.context.bindService(new Intent(this.context, P2ChatService.class), serviceConnection, Context.BIND_AUTO_CREATE);
-  }
-
-  public static Matrix getMatrixInstance() {
-    return matrixInstance;
-  }
-
-  public static Realm getCredsRealmInstance() {
-    RealmConfiguration realmCredsConfig = new RealmConfiguration.Builder()
-            .name("realm.creds")
-            //.encryptionKey() // TODO: DB encryption
-            .schemaVersion(0)
-            //.migration(new MyMigration()) // TODO: DB migration
-            .build();
-    return Realm.getInstance(realmCredsConfig);
+    this.context.bindService(new Intent(this.context, P2ChatService.class), serviceConnection, Context.BIND_AUTO_CREATE);
   }
 
   //==============================================================================================================
