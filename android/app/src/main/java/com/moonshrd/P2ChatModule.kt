95774e2572e7db2905afeb5b147e57054103b145
ChronosX88 <chronosx88@gmail.com>
2019-09-21 21:00:24 +0400

    feat: Implement storing data about local chats

--- android/app/src/main/java/com/moonshrd/P2ChatModule.kt
+++ android/app/src/main/java/com/moonshrd/P2ChatModule.kt
@@ -5,12 +5,18 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.google.gson.Gson
+import com.moonshrd.models.LocalChat
+import com.moonshrd.models.Message
+import com.moonshrd.repository.LocalChatsRepository
 import com.moonshrd.utils.TopicStorage
+import com.moonshrd.utils.matrix.Matrix
+import java.util.*
 import javax.inject.Inject
 
 class P2ChatModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     @Inject lateinit var gson: Gson
     @Inject lateinit var topicStorage: TopicStorage
+    @Inject lateinit var matrixInstance: Matrix
 
     init {
         MainApplication.getComponent().injectsP2ChatModule(this)
@@ -24,6 +30,7 @@ class P2ChatModule(reactContext: ReactApplicationContext) : ReactContextBaseJava
     fun subscribeToTopic(topic: String, promise: Promise) {
         MainApplication.getP2ChatService().subscribeToTopic(topic)
         topicStorage.addTopic(topic)
+        LocalChatsRepository.addLocalChat(topic, LocalChat())
         promise.resolve(topic)
     }
 
@@ -31,6 +38,7 @@ class P2ChatModule(reactContext: ReactApplicationContext) : ReactContextBaseJava
     fun unsubscribeFromTopic(topic: String, promise: Promise) {
         MainApplication.getP2ChatService().unsubscribeFromTopic(topic)
         topicStorage.removeTopic(topic)
+        LocalChatsRepository.removeLocalChat(topic)
         promise.resolve(true)
     }
 
@@ -41,14 +49,20 @@ class P2ChatModule(reactContext: ReactApplicationContext) : ReactContextBaseJava
     }
 
     @ReactMethod
-    fun getAllMatches(promise: Promise) {
-        val matchesMap = MainApplication.getP2ChatService().getAllMatches()
-        promise.resolve(if (matchesMap != null) gson.toJson(matchesMap) else "")
+    fun sendMessage(topic: String, messageText: String, promise: Promise) {
+        MainApplication.getP2ChatService().sendMessage(topic, messageText)
+        val from = matrixInstance.defaultSession?.myUserId ?: ""
+        LocalChatsRepository.getLocalChat(topic)!!.putMessage(Message(UUID.randomUUID().toString(), messageText, from, topic, System.currentTimeMillis()))
+        promise.resolve(true)
     }
 
     @ReactMethod
-    fun sendMessage(topic: String, messageText: String, promise: Promise) {
-        MainApplication.getP2ChatService().sendMessage(topic, messageText)
-        promise.resolve(true)
+    fun getLocalChatMembersByTopic(topic: String, promise: Promise) {
+        promise.resolve(gson.toJson(LocalChatsRepository.getLocalChat(topic)!!.getMembers()))
+    }
+
+    @ReactMethod
+    fun loadMoreMessages(topic: String, paginationToken: String, promise: Promise) {
+        promise.resolve(gson.toJson(LocalChatsRepository.getLocalChat(topic)!!.getHistoryMessages(paginationToken)))
     }
 }
